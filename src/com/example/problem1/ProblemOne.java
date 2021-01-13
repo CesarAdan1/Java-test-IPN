33b58461f83584232aaa84544f90610899502707
Cesar Adan <whoclash@hotmail.com>
2021-01-13 13:26:32 -0600

    1

--- src/com/example/problem1/ProblemOne.java
+++ src/com/example/problem1/ProblemOne.java
@@ -5,35 +5,30 @@ import java.util.Arrays;
 
 public class ProblemOne {
 
-    static int RecursiveFunction(int r, int n){
-        Scanner input = new Scanner(System.in);
-        //int resultado = RecursiveFunction();
-        System.out.println("Ingresa la cantidad de coeficientes a calcular ");
-        n = input.nextInt();
-        System.out.println("Ingresa un valor para r");
-        r = input.nextInt();
-
-        int x = n;
-        int res = x;
-
-
-        int recursive = (RecursiveFunction(n - 1, r) + RecursiveFunction(n, n - 1));
-
-       // if (x == -x) return  0;
-         if(r >= 0){
-
-                    return (RecursiveFunction(n - 1, r) + RecursiveFunction(n, n - 1));
-                   // else if (r == 0 && n >= 3)
-                      //  return (n * RecursiveFunction(n - 1, r)) / (x * RecursiveFunction(x - 1, r)) * (RecursiveFunction((n - x) - 1, r) + RecursiveFunction((n - x) - 1, r));
-                }
-            return res;
-    }
-
     public static void main(String[] args){
+
         int r = 0;
         int n = 0; //N
+        int result = recursiveFunction(r, n);
 
-        System.out.println(RecursiveFunction(r, n));
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingresa la cantidad de coeficientes a calcular ");
+        n = input.nextInt();
 
+        System.out.println("Ingresa un valor para r");
+        r = input.nextInt();
+        System.out.println("Los coeficientes son: " + result);
+
+    }
+
+    public static int recursiveFunction(int n, int r){
+        if (r >= 0 && n >= 0){
+            //if(r < 0) return 0;
+                //else
+            return (recursiveFunction(n - 1, r) + recursiveFunction(n, r - 1));
+
+         } else {
+            return n;
+        }
     }
 }
