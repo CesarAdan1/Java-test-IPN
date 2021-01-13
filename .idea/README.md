99de8fe546df7249a6efb2b1e980c1ca4decbbd7
Cesar Adan <whoclash@hotmail.com>
2021-01-13 12:00:45 -0600

    readme

---
+++ .idea/README.md
@@ -0,0 +1,4 @@
+# This project was making with IntelliJ
+## The source code is in src/com.example.problem1/ProblemOne
+
+### Copy the source code onto another IDE or online editor containing in the previos written path
--- src/com/example/problem1/ProblemOne.java
+++ src/com/example/problem1/ProblemOne.java
@@ -10,29 +10,30 @@ public class ProblemOne {
         //int resultado = RecursiveFunction();
         System.out.println("Ingresa la cantidad de coeficientes a calcular ");
         n = input.nextInt();
-
-
-        int[] x = {n};
-        int[] res = x;
-
         System.out.println("Ingresa un valor para r");
         r = input.nextInt();
 
-        if(r >= 0 && n >= 3)  return (RecursiveFunction(x - 1, r) + RecursiveFunction(x, x - 1));
-            else if(x < 0) return x = [0] ;
-            else if(r == 0 && n >= 3) return (n * RecursiveFunction(n - 1, r))/( x * RecursiveFunction(x - 1, r)) * (RecursiveFunction((n - x) -1, r) + RecursiveFunction((n-x) - 1, r)) ;
+        int x = n;
+        int res = x;
 
-            return res[];
+
+        int recursive = (RecursiveFunction(n - 1, r) + RecursiveFunction(n, n - 1));
+
+       // if (x == -x) return  0;
+         if(r >= 0){
+
+                    return (RecursiveFunction(n - 1, r) + RecursiveFunction(n, n - 1));
+                   // else if (r == 0 && n >= 3)
+                      //  return (n * RecursiveFunction(n - 1, r)) / (x * RecursiveFunction(x - 1, r)) * (RecursiveFunction((n - x) - 1, r) + RecursiveFunction((n - x) - 1, r));
+                }
+            return res;
     }
 
     public static void main(String[] args){
         int r = 0;
         int n = 0; //N
-        int x = 0;
-        int[] response;
-        int operacion;
 
-        RecursiveFunction(r, n);
+        System.out.println(RecursiveFunction(r, n));
 
     }
 }
